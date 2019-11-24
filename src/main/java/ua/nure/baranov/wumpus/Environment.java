ea3a1ca77f95ec905a37678e4184e3dbcbb76a60
Yevhenii Baranov <yevhenii.baranov@teamdev.com>
2019-11-24 15:53:56 +0200

    Find environment and navigator from the yellow pages.

--- src/main/java/ua/nure/baranov/wumpus/Environment.java
+++ src/main/java/ua/nure/baranov/wumpus/Environment.java
@@ -3,21 +3,46 @@ package ua.nure.baranov.wumpus;
 import jade.core.AID;
 import jade.core.Agent;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class Environment extends Agent {
 
-    private Field[][] gameField;
+    private final int caveXDimension;
+    private final int caveYDimension;
+
 
     private AID speleologistId;
     private Coordinates speleologistCoordinates;
     private Direction speleologistDirection;
 
-    private List<Coordinates> pitsCoordinates;
+    private Room wumpus;
+    private Room gold;
+    private Set<Room> pits = new LinkedHashSet<>();
 
-    private List<Coordinates> goldCoordinates;
-    private List<Coordinates> wumpusCoordinates;
+    private Set<Room> allowedRooms;
 
     private long currentTimeTact;
 
+    public Environment(int caveXDimension, int caveYDimension) {
+        if (caveXDimension < 1)
+            throw new IllegalArgumentException("Cave must have x dimension >= 1");
+        if (caveYDimension < 1)
+            throw new IllegalArgumentException("Case must have y dimension >= 1");
+        this.caveXDimension = caveXDimension;
+        this.caveYDimension = caveYDimension;
+        allowedRooms = getAllRooms();
+    }
+
+    private Set<Room> getAllRooms() {
+        Set<Room> rooms = new HashSet<>();
+        for (int i = 0; i < caveXDimension; i++) {
+            for (int j = 0; j < caveYDimension; j++) {
+                Room room = new Room(i, j);
+                rooms.add(room);
+            }
+        }
+        return rooms;
+    }
 }
--- src/main/java/ua/nure/baranov/wumpus/Speleologist.java
+++ src/main/java/ua/nure/baranov/wumpus/Speleologist.java
@@ -3,11 +3,14 @@ package ua.nure.baranov.wumpus;
 import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.Behaviour;
-import jade.core.behaviours.CyclicBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
 
-import static jade.lang.acl.MessageTemplate.*;
+import static jade.lang.acl.MessageTemplate.MatchInReplyTo;
 
 public class Speleologist extends Agent {
 
@@ -15,18 +18,57 @@ public class Speleologist extends Agent {
     private AID environment;
     private WumpusAction lastAction;
     private static final String SPELEOLOGIST_NAVIGATOR_CONVERSATION_ID = "speleologist-navigator";
+    private static final String SPELEOLOGIST_ENVIRONMENT_CONVERSATION_ID = "speleologist-environment";
     private MessageTemplate expectedMessageTemplate;
 
     @Override
     protected void setup() {
-        addBehaviour(new RequestBehavior());
+        registerAgent();
+        String environmentType = "wumpus-environment";
+        String navigatorType = "wumpus-navigator";
 
+        this.environment = findService(environmentType);
+        this.navigator = findService(navigatorType);
 
+        addBehaviour(new SpeleologistBehaviour());
     }
 
-    private class RequestBehavior extends Behaviour {
-        private int step = 0;
+    private AID findService(String serviceType) {
+        final DFAgentDescription templateDescription = new DFAgentDescription();
+        final ServiceDescription serviceDescription = new ServiceDescription();
 
+        serviceDescription.setType(serviceType);
+        templateDescription.addServices(serviceDescription);
+
+        try {
+            final DFAgentDescription[] result = DFService.search(this, templateDescription);
+            if (result.length != 1) {
+                throw new RuntimeException("Size of found agents does not equal to 1!");
+            }
+            return result[0].getName();
+
+        } catch (FIPAException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    private void registerAgent() {
+        final DFAgentDescription description = new DFAgentDescription();
+        description.setName(this.getAID());
+        final ServiceDescription serviceDescription = new ServiceDescription();
+        serviceDescription.setType("book-selling");
+        serviceDescription.setName("JADE-quickstart-book-seller");
+        description.addServices(serviceDescription);
+        //todo update for speleologist
+        try {
+            DFService.register(this, description);
+        } catch (FIPAException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class SpeleologistBehaviour extends Behaviour {
+        private int step = 0;
 
         @Override
         public void action() {
@@ -34,7 +76,7 @@ public class Speleologist extends Agent {
                 case 0: {
                     ACLMessage aclMessage = new ACLMessage(ACLMessage.REQUEST);
 
-                    aclMessage.setConversationId(SPELEOLOGIST_NAVIGATOR_CONVERSATION_ID);
+                    aclMessage.setConversationId(SPELEOLOGIST_ENVIRONMENT_CONVERSATION_ID);
 
                     String replyMarker = String.format("env req %d", System.currentTimeMillis());
                     expectedMessageTemplate = MatchInReplyTo(replyMarker);
@@ -57,6 +99,7 @@ public class Speleologist extends Agent {
                         String replyMarker = String.format("nav req %d", System.currentTimeMillis());
                         expectedMessageTemplate = MatchInReplyTo(replyMarker);
 
+                        navigatorRequest.setConversationId(SPELEOLOGIST_NAVIGATOR_CONVERSATION_ID);
                         navigatorRequest.setReplyWith(replyMarker);
                         navigatorRequest.addReceiver(navigator);
                         this.myAgent.send(navigatorRequest);
@@ -71,7 +114,7 @@ public class Speleologist extends Agent {
                     ACLMessage response = myAgent.receive(expectedMessageTemplate);
                     if (response != null) {
                         lastAction = null;
-                        // TODO: 11/24/2019  
+                        // TODO: 11/24/2019
 
                         ACLMessage callForProposal = new ACLMessage(ACLMessage.CFP);
                         callForProposal.addReceiver(environment);
@@ -80,6 +123,7 @@ public class Speleologist extends Agent {
                         String replyMarker = String.format("env cfp %d", System.currentTimeMillis());
                         expectedMessageTemplate = MatchInReplyTo(replyMarker);
 
+                        callForProposal.setConversationId(SPELEOLOGIST_ENVIRONMENT_CONVERSATION_ID);
                         callForProposal.setReplyWith(replyMarker);
                         this.myAgent.send(callForProposal);
                         step = 3;
@@ -120,14 +164,6 @@ public class Speleologist extends Agent {
         @Override
         public boolean done() {
             return step == 4;
-        }
-    }
-
-    private class ProposalBehavior extends CyclicBehaviour {
-
-        @Override
-        public void action() {
-
         }
     }
 }
